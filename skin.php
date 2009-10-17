a91bf617dea6d1f60ab8a0e38bdf26d61448ff97
David Gilbert <david@solidgone.com>
2009-10-17 22:02:09 +0000

    chg: Changed footer to point to blogit page, if blogit is being used. bug: Unable to turn off search bar or sidebar. new: Added class .button to allow user styling of form elements. new: Added variables to allow fluid or user-specified width ($SkinWidth,$SkinSidebarWidth,$SkinWidthUnit).

--- skin.php
+++ skin.php
@@ -18,6 +18,14 @@ if (!empty($PageLogoUrl)) {
 		'#logo .sitetitle a{height:' .$PageLogoUrlHeight .'; background: url(' .$PageLogoUrl .') left 16px no-repeat} '.
 		'#logo .sitetitle a, #logo .sitetag{padding-left: ' .$PageLogoUrlWidth .'} ';
 }
+global $SkinWidth,$SkinSidebarWidth,$SkinWidthUnit;
+SDV($SkinWidth,850);
+SDV($SkinSidebarWidth,195);  #good percentage width is 25
+SDV($SkinWidthUnit,'px');  #only use 'px' or '%'
+$HTMLStylesFmt['colorimetry'] .=	'#header, #page, #footer-inner { width: '.$SkinWidth.$SkinWidthUnit.'; } '.
+	'#content { width: '.($SkinWidthUnit=='px'?($SkinWidth-$SkinSidebarWidth-55) :(100-$SkinSidebarWidth-5)) .$SkinWidthUnit.'; } '.
+	'#sidebar { width: '.$SkinSidebarWidth .$SkinWidthUnit.'; } ';
+
 $SkinColor = dg_SetSkinColor('green', array('green','orange','blue','purple','yellow','pink'));
 
 # ----------------------------------------
@@ -27,6 +35,8 @@ $FmtPV['$WikiTitle'] = '$GLOBALS["WikiTitle"]';
 $FmtPV['$WikiTag'] = '$GLOBALS["WikiTag"]';
 
 # Move any (:noleft:) or SetTmplDisplay('PageLeftFmt', 0); directives to variables for access in jScript.
+$FmtPV['$LeftColumn'] = "\$GLOBALS['TmplDisplay']['PageLeftFmt']";
+Markup('noleft', 'directives',  '/\\(:noleft:\\)/ei', "SetTmplDisplay('PageLeftFmt',0)");
 $FmtPV['$RightColumn'] = "\$GLOBALS['TmplDisplay']['PageRightFmt']";
 Markup('noright', 'directives',  '/\\(:noright:\\)/ei', "SetTmplDisplay('PageRightFmt',0)");
 $FmtPV['$ActionBar'] = "\$GLOBALS['TmplDisplay']['PageActionFmt']";
@@ -65,7 +75,9 @@ global $SkinColor, $ValidSkinColors, $_GET;
 		$SkinColor = $default;
 	return $SkinColor;
 }
-
+function dg_PoweredBy(){
+	print ('<a href="http://pmwiki.com/'.($GLOBALS['bi_BlogIt_Enabled']?'Cookbook/BlogIt">BlogIt':'">PmWiki').'</a>');
+}
 # Determine logo height and width
 function dg_SetLogoHeightWidth ($wPad, $hPad=0){
 global $PageLogoUrl, $PageLogoUrlHeight, $PageLogoUrlWidth;
